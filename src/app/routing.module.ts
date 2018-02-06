89f02a439453f44ac17b1dedd6d2739b38474d84
vanvani4 <iiiordanov@gmail.com>
2018-02-06 21:07:17 +0200

    10

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -9,11 +9,11 @@ import { AuthGuard } from './guard/auth-guard.service';
 import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: 'api', pathMatch: 'full', canActivate: [AuthGuard]},
-    {path: 'api', component: SearchPersonComponent, pathMatch: 'full', canActivate: [AuthGuard]},
+    {path: '', redirectTo: 'api', pathMatch: 'full'/*, canActivate: [AuthGuard]*/},
+    {path: 'api', component: SearchPersonComponent, pathMatch: 'full'/*, canActivate: [AuthGuard]*/},
     {path: 'api/login', component: LoginComponent, pathMatch: 'full'},
-    {path: 'api/add', component: AddPersonComponent, pathMatch: 'full', canActivate: [AuthGuard]},
-    {path: 'api/:id', component: AboutPersonComponent, pathMatch: 'full', canActivate: [AuthGuard]}
+    {path: 'api/add', component: AddPersonComponent, pathMatch: 'full'/*, canActivate: [AuthGuard]*/},
+    {path: 'api/:id', component: AboutPersonComponent, pathMatch: 'full'/*, canActivate: [AuthGuard]*/}
 ];
 
 @NgModule({
