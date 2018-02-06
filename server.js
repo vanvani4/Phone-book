b12c412275ce7a5fd8a317573a638f99ecf4dc4d
vanvani4 <iiiordanov@gmail.com>
2018-02-06 22:28:18 +0200

    12

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const http = require('http');
 const api = require('./server/routes/api');
 // const photo = require('./server/photo_person');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://***.herokuapp.com:443/api');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, token, authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+ });
 
 app.use(bodyParser.json());//
 app.use(bodyParser.urlencoded({extended: false}));//
