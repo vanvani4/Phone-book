43bb628362f7cd10ddcf25a589e0c634fe02449b
vanvani4 <iiiordanov@gmail.com>
2018-02-07 20:38:06 +0200

    17

--- server/routes/api.js
+++ server/routes/api.js
@@ -65,6 +65,8 @@ router.post('/add', upload.single('photo'), function(req, res, next) {
   console.log('POST ADD!');
   person.push({name: req.body.name, phone: req.body.phone, id: count});
   count++;
+  console.log('storage', storage);
+  console.log('dirname', __dirname);
 //   cloudinary.uploader.upload(req.file.path, function(result) {
 //     person.push({name: req.body.name, phone: req.body.phone, photo: result.url});
 // });
